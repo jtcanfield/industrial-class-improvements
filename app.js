0971b6a165b0944975136d931f11d8e7f0208a5a
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-01-05 22:48:54 -0500

    add eslint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var logger = require('morgan');
 
 const app = express();
 
