34eb91d9fd7387cb243f12727870890d2ec41f1e
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-01-06 03:01:14 -0500

    move views into server

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { User } = require('./server/schemas');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
