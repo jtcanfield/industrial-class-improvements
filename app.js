a801f53705c2b1e019d70472e64022813e8c95d2
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-03-18 16:45:12 -0400

    add paypal sdk

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ const expressSession = require('express-session')({
   resave: false,
   saveUninitialized: false,
 });
+const paypal = require('paypal-rest-sdk');
 const router = require('./server/routes');
 const { User } = require('./server/schemas');
+
+paypal.configure({
+  mode: 'live', // TODO Might change?
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
+});
 
 const app = express();
 
@@ -36,8 +43,11 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use((req, res, next) => {
+  req.paypal = paypal;
+  next();
+});
 app.use('/', router);
 
 app.get('*', (req, res) => {
